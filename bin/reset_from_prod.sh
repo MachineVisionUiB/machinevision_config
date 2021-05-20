b67efc2a7f187b9f7eab193a72e8d783d8abd920
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2021-05-20 02:10:02 +0200

    Force decompress database backup during staging

--- bin/reset_from_prod.sh
+++ bin/reset_from_prod.sh
@@ -9,7 +9,7 @@ rsync -av mvision:/var/www/drupal/machinevision/latest.sql.gz /Users/steinmb/sit
 # Nuke old db. and import from prod. snapshot.
 cd /Users/steinmb/sites/machinevision/machinevision/web || exit
 ../vendor/bin/drush sql-drop --yes
-gunzip /Users/steinmb/sites/machinevision/latest.sql.gz
+gunzip --force /Users/steinmb/sites/machinevision/latest.sql.gz
 mysql mvisiond8 < ../../latest.sql
 
 # Clear caches and authenticate.
