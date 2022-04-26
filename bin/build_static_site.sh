3d7db71b6f7c1256528e036703dd28508b7f2f9b
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-04-26 13:43:36 +0200

    Improve static build
    - Remove public search permission
    - More auto staging to build machine

--- bin/build_static_site.sh
+++ bin/build_static_site.sh
@@ -5,4 +5,11 @@
 
 cd web || exit 1
 
-../vendor/bin/drush tome:static --uri=http://static.machinevision.local
+cp -v ../bin/views.view.search.yml ../config/sync
+../vendor/bin/drush sql-cli
+../vendor/bin/drush config:import --yes
+../vendor/bin/drush sql-dump --result-file=../bin/static_db.sql
+
+rsync -a -v ../../machinevision cloud-user@158.39.77.25:/home/cloud-user/
+
+#../vendor/bin/drush tome:static -v --uri=https://machinevisionuib.github.io
---
+++ bin/sync_back_static_site.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rsync -a -v cloud-user@158.39.77.25:/home/cloud-user/static_version /Users/steinmb/sites/machinevision
+
