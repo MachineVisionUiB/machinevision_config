9e9e9020f661bc03f99d4778d608768990a6948b
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-04-26 13:53:38 +0200

    Do not include dir name in rsync

--- bin/sync_back_static_site.sh
+++ bin/sync_back_static_site.sh
@@ -2,7 +2,7 @@
 
 cd ../../machinevision || exit 1
 # rsync -a -v cloud-user@158.39.77.25:/home/cloud-user/static_version .
-rsync -a -v cloud-user@158.39.77.25:/home/cloud-user/static_version/html machinevisionuib.github.io/
+rsync -a -v cloud-user@158.39.77.25:/home/cloud-user/static_version/html/ machinevisionuib.github.io/
 
 cd machinevisionuib.github.io || exit 1
 git add .
