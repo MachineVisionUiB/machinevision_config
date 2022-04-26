44e98156f1cc6bf34191fc0dde89a65c00c702ea
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-04-26 15:22:33 +0200

    Exit if search override does not exist
    - Build takes forever if not and search does not work anyway.

--- bin/build_static_site.sh
+++ bin/build_static_site.sh
@@ -3,10 +3,9 @@
 # Export all content til JSON files and create
 # a static HTML version of the site.
 
-cd web || exit 1
+cp -v bin/views.view.search.yml config/sync || exit 1
 
-cp -v ../bin/views.view.search.yml ../config/sync
-../vendor/bin/drush sql-cli
+cd web || exit 1
 ../vendor/bin/drush config:import --yes
 ../vendor/bin/drush sql-dump --result-file=../bin/static_db.sql
 
