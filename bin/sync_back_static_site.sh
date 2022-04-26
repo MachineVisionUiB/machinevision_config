7ffe12b3bf842b13e46f7206847b2471dc753f7f
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-04-26 13:52:20 +0200

    For now ignore full rsync of static build

--- bin/sync_back_static_site.sh
+++ bin/sync_back_static_site.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ../../machinevision || exit 1
-rsync -a -v cloud-user@158.39.77.25:/home/cloud-user/static_version .
+# rsync -a -v cloud-user@158.39.77.25:/home/cloud-user/static_version .
 rsync -a -v cloud-user@158.39.77.25:/home/cloud-user/static_version/html machinevisionuib.github.io/
 
 cd machinevisionuib.github.io || exit 1
