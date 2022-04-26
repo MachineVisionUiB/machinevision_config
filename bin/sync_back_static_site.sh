6f9f899ab4544d28b63e0bf985e3add72e8416ad
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-04-26 13:50:31 +0200

    Improve static build
    - More auto staging to build machine

--- bin/sync_back_static_site.sh
+++ bin/sync_back_static_site.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-rsync -a -v cloud-user@158.39.77.25:/home/cloud-user/static_version /Users/steinmb/sites/machinevision
+cd ../../machinevision || exit 1
+rsync -a -v cloud-user@158.39.77.25:/home/cloud-user/static_version .
+rsync -a -v cloud-user@158.39.77.25:/home/cloud-user/static_version/html machinevisionuib.github.io/
 
+cd machinevisionuib.github.io || exit 1
+git add .
+git commit -m "Update build"
+git push origin
