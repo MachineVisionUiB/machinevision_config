3a55dc4b6b6949001ed0a4af245bd8caeb989715
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-08-17 09:06:15 +0200

    Remove hardcoded paths where we can
    - Remove local hardcoded paths.
    - Avoid repeating paths and domain names.

--- bin/reset_from_prod.sh
+++ bin/reset_from_prod.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# File sync, images, video etc.
-rsync -av --exclude=bootstrap --exclude=ctools --exclude=styles --exclude=php --exclude=css --exclude=js mvision:/var/www/drupal/machinevision/machinevision/web/sites/default/files/ /Users/steinmb/sites/machinevision/machinevision/web/sites/default/files
+LIVE_SITE=machine-vision.no
+HOMEDIR=${PWD}
+LIVE_HOMEDIR=/var/www/drupal/machinevision/machinevision
+
+# File sync of user generated images, videos etc.
+rsync -av --dry-run \
+--exclude=bootstrap --exclude=ctools --exclude=styles --exclude=php --exclude=css --exclude=js \
+$LIVE_SITE:"$LIVE_HOMEDIR"/web/sites/default/files/ \
+"$HOMEDIR"/web/sites/default/files
 
 # Grab live database.
-rsync -av mvision:/var/www/drupal/machinevision/latest.sql.gz /Users/steinmb/sites/machinevision
+rsync -av $LIVE_SITE:"$LIVE_HOMEDIR"/../latest.sql.gz "$HOMEDIR"/..
 
-# Nuke old db. and import from prod. snapshot.
-cd /Users/steinmb/sites/machinevision/machinevision/web || exit
+# Nuke old db. and import db dump from from production.
+cd "$HOMEDIR"/web || exit
 ../vendor/bin/drush sql-drop --yes
-gunzip --force /Users/steinmb/sites/machinevision/latest.sql.gz
+gunzip --force "$HOMEDIR"/../latest.sql.gz
 mysql mvisiond8 < ../../latest.sql
 
 # Clear caches and authenticate.
